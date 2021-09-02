ea591b3df5e28330eec53a58ebd06fcb69b4a7ab
Zoheb Ahmed <zohebcool1542@gmail.com>
2021-09-02 17:44:37 +0530

    initial changes

--- script1.js
+++ script1.js
@@ -3,8 +3,6 @@ const startScreen = document.querySelector('.startScreen')
 const gameArea = document.querySelector('.gameArea')
 var flag = 0
 
-//console.log(stress_lvl);
-
 startScreen.addEventListener('click', start)
 
 let player = { speed: 9, stress_lvl: 0 }
@@ -22,14 +20,12 @@ document.addEventListener('keyup', keyUp)
 function keyDown(e) {
   e.preventDefault()
   keys[e.key] = true
-  //console.log(e.key);
   console.log(keys)
 }
 
 function keyUp(e) {
   e.preventDefault()
   keys[e.key] = false
-  //console.log(e.key);
 }
 
 function isCollide(a, b) {
@@ -65,16 +61,11 @@ function moveEnemy(car) {
     let enemy = document.querySelectorAll('.enemy' + x)
     enemy.forEach(function (item) {
       if (isCollide(car, item)) {
-        //if(flag==0){
-        //flag=1;
         console.log('Boom HIT!')
         player.stress_lvl += 100
-        //alert("stress_lvl: " + player.stress_lvl);
         stress_lvl.innerText = 'stress_lvl: ' + player.stress_lvl
 
         item.classList.add('hide')
-
-        //}
       }
 
       if (item.y >= 750) {
@@ -93,11 +84,9 @@ function booster(car) {
     let boost = document.querySelectorAll('.boost' + x)
     boost.forEach(function (item) {
       if (isCollide(car, item)) {
-        //console.log("Received BOOST!!");
         if (player.stress_lvl > 0) {
           player.stress_lvl -= 25
         }
-        //alert("stress_lvl: " + player.stress_lvl);
         stress_lvl.innerText = 'stress_lvl: ' + player.stress_lvl
         item.classList.add('hide')
       }
@@ -122,12 +111,8 @@ function endgame() {
 }
 
 function gamePlay() {
-  //console.log('Hey I am clicked');
-  //console.log('player.start = ',player.start);
-  //console.log(car);
   let car = document.querySelector('.car')
   let road = gameArea.getBoundingClientRect() // for finding out the position of the road (boundary conditions).
-  //console.log(road);
 
   if (player.start) {
     if (player.stress_lvl > 500) {
@@ -153,16 +138,12 @@ function gamePlay() {
     car.style.left = player.x + 'px'
 
     window.requestAnimationFrame(gamePlay)
-    //console.log(player.stress_lvl++);
 
-    //player.stress_lvl-= 0.25;
     stress_lvl.innerText = 'Stress Level \n' + player.stress_lvl
   }
 }
 
 function start() {
-  //console.log('Start Prog started');
-  //gameArea.classList.remove('hide');
   startScreen.classList.add('hide')
   gameArea.innerHTML = ''
 
@@ -180,17 +161,12 @@ function start() {
 
   let car = document.createElement('div')
   car.setAttribute('class', 'car')
-  //car.innerText="Yo I am your car";
   gameArea.appendChild(car)
 
   player.x = car.offsetLeft
   player.y = car.offsetTop
 
-  //console.log("top position"+ car.offsetTop);
-  //console.log("left position"+ car.offsetLeft);
-
   for (x = 0; x < 3; x++) {
-    //ChangeIt();
     let enemyCar = document.createElement('div')
     enemyCar.setAttribute('class', 'enemy' + x)
     console.log('enemy' + x)
@@ -208,10 +184,3 @@ function start() {
     gameArea.appendChild(booster)
   }
 }
-
-/*function ChangeIt() 
-{
-var num = Math.ceil( Math.random() * 5 );
-console.log('THis is the no', num);
-enemy.body.background = '3.jpg';
-}*/
